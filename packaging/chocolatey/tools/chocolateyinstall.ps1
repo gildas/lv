f3f8f9258034094b96173abd6cf839d8aef7c7d3
Gildas Cherruel <gildas@breizh.org>
2025-05-03 09:28:34 +0900

    Updated packaging

--- packaging/chocolatey/tools/chocolateyinstall.ps1
+++ packaging/chocolatey/tools/chocolateyinstall.ps1
@@ -5,9 +5,9 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe'
-  file64        = "$toolsDir\lv.2.0-windows-amd64.7z"
+  file64        = "$toolsDir\bunyan-logviewer-0.3.0-windows-amd64.7z"
   softwareName  = 'lv*'
-  checksum64    = ''
+  checksum64    = '6d80479623d3c2b578558e4f1bec896fe4f8fac1205e2e7cfe2b0d267f09ecac'
   checksumType64= 'sha256'
 }
 
